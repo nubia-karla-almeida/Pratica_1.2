ff55deeb0ee74c80078642f2a0cfd51aa3334d39
nubia-karla-almeida <nubia_karla@id.uff.br>
2026-08-24 09:40:45 -0300

    script turismo

---
+++ script_turismo_branch.R
@@ -0,0 +1,14 @@
+# Etapa 1: Leitura e manipulação dos dados na branch “main”
+# Leitura e verificação do banco
+
+# Atribuindo legendas dos rótulos das variáveis qualitativas
+
+
+# Etapa 2: Análise descritiva de dados na branch “analise-descritiva”
+# Tabelas e gráficos de distribuição de frequências das variáveis qualitativas
+
+# Medidas descritivas para variáveis quantitativas
+
+
+# Etapa 3: Análise inferencial na branch “analise-inferencial”
+# Teste de comparação de médias das variáveis quantitativas por dois grupos de variáveis qualitativas
